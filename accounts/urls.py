a59306139bc976f45e1ea0de2e369b9f5e2e25d1
dav010203 <davidsimonyan460@gmail.com>
2024-01-16 21:56:13 +0400

    customer and vendor registration and dashboard

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('registerUser/', views.register_user, name='registerUser'),
     path('registerVendor/', views.register_vendor, name='registerVendor'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('myAccount/', views.myAccount, name='myAccount'),
+    path('custDashboard/', views.custDashboard, name='custDashboard'),
+    path('vendorDashboard/', views.vendorDashboard, name='vendorDashboard'),
 ]
--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -140,6 +140,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 from django.contrib.messages import constants as messages
-MESSAGES_TAGS = {
+MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
