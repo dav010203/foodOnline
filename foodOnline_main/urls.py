636173cf51ff9f8cb00f20bcf2211fda33379820
dav010203 <davidsimonyan460@gmail.com>
2024-01-30 23:33:15 +0400

    cart functionality

--- foodOnline_main/urls.py
+++ foodOnline_main/urls.py
@@ -19,9 +19,13 @@ from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from marketplace import views as marketplace_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('', include('accounts.urls')),
+    path('marketplace/', include('marketplace.urls')),
+    # CART
+    path('cart/', marketplace_views.cart, name='cart'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
