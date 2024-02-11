5472f319433ef621e3f9c00697e4857f4450f6ba
dav010203 <davidsimonyan460@gmail.com>
2024-02-11 20:58:02 +0400

    Business opening hours feature

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render, get_object_or_404
-from vendor.models import Vendor
+from vendor.models import Vendor, OpeningHour
 from menu.models import Category, FoodItem
 from django.db.models import Prefetch
 from django.http import JsonResponse
@@ -10,6 +10,7 @@ from django.db.models import Q
 from django.contrib.gis.geos import GEOSGeometry
 from django.contrib.gis.measure import D
 from django.contrib.gis.db.models.functions import Distance
+from datetime import date, datetime
 
 # Create your views here.
 def marketplace(request):
@@ -32,7 +33,14 @@ def vendor_detail(request, vendor_slug):
             queryset = FoodItem.objects.filter(is_available=True)
         )
     )
+
+    opening_hours = OpeningHour.objects.filter(vendor=vendor).order_by('day', '-from_hour')
     
+    # Check current days' opening hours.
+    today_date = date.today()
+    today = today_date.isoweekday()
+    current_opening_hours = OpeningHour.objects.filter(vendor=vendor, day=today)
+
     if request.user.is_authenticated:
         cart_items = Cart.objects.filter(user=request.user)
     else:
@@ -42,6 +50,8 @@ def vendor_detail(request, vendor_slug):
         'vendor': vendor,
         'categories': categories,
         'cart_items': cart_items,
+        'opening_hours': opening_hours,
+        'current_opening_hours': current_opening_hours,
     }
     return render(request, 'marketplace/vendor_detail.html', context)
 
--- vendor/models.py
+++ vendor/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from accounts.models import User, UserProfile
 from accounts.utils import send_notification
+from datetime import time, date, datetime
 
 # Create your models here.
 class Vendor(models.Model):
@@ -15,6 +16,27 @@ class Vendor(models.Model):
 
     def __str__(self):
         return self.vendor_name
+    
+    def is_open(self):
+        # Check current days' opening hours.
+        today_date = date.today()
+        today = today_date.isoweekday()
+        current_opening_hours = OpeningHour.objects.filter(vendor=self, day=today)
+        now = datetime.now()
+        current_time = now.strftime('%H:%M:%S')
+
+        is_open = None
+        for hour in current_opening_hours:
+            start = str(datetime.strptime(hour.from_hour, '%I:%M %p').time())
+            end = str(datetime.strptime(hour.to_hour, '%I:%M %p').time())
+            if current_time > start and current_time < end:
+                is_open = True
+                break
+            else:
+                is_open = False
+
+        return is_open
+    
     
     def save(self, *args, **kwargs):
         if self.pk is not None:
@@ -37,3 +59,30 @@ class Vendor(models.Model):
 
         return super(Vendor, self).save(*args, **kwargs)
     
+
+DAYS = [
+    (1, ('Monday')),
+    (2, ('Tuesday')),
+    (3, ('Wednesday')),
+    (4, ('Thursday')),
+    (5, ('Friday')),
+    (6, ('Saturday')),
+    (7, ('Sunday')),
+]
+
+HOUR_OF_DAY_24 = [(time(h, m).strftime('%I:%M %p'), time(h, m).strftime('%I:%M %p')) for h in range(0, 24) for m in (0, 30)]
+
+
+class OpeningHour(models.Model):
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    day = models.IntegerField(choices=DAYS)
+    from_hour = models.CharField(choices=HOUR_OF_DAY_24, max_length=10, blank=True)
+    to_hour = models.CharField(choices=HOUR_OF_DAY_24, max_length=10, blank=True)
+    is_closed = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('day', '-from_hour')
+        unique_together = ('vendor', 'day', 'from_hour', 'to_hour')
+
+    def __str__(self):
+        return self.get_day_display()
