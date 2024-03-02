1697c80b0c083271dce94273eaa8f9ebdc99bf25
dav010203 <davidsimonyan460@gmail.com>
2024-03-02 11:26:21 +0400

    deploy to linode server

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.233.223.73', '127.0.0.1', 'arm-foodonline.com', 'www.arm-foodonline.com']
 
 
 # Application definition
