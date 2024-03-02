f4bf0577d4cf113d1bc64aa48786ecc1e3e237b4
dav010203 <davidsimonyan460@gmail.com>
2024-03-02 17:25:37 +0400

    uncomment post save signal

--- accounts/signals.py
+++ accounts/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from .models import User, UserProfile
     
-"""
+
 @receiver(post_save, sender=User)
 def post_save_create_profile_receiver(sender, instance, created, **kwargs):
     print(created)
@@ -15,7 +15,7 @@ def post_save_create_profile_receiver(sender, instance, created, **kwargs):
         except:
             # Create the user profile if does not exist
             UserProfile.objects.create(user=instance)
-"""
+
              
 
 @receiver(pre_save, sender=User)
